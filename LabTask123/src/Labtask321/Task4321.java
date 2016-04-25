1259422bd5cdf5c6dc68d2b5b91ee5cd8ea8966d
SOHAIL54321 <sohailajmera1@gmail.com>
2016-04-25 17:41:15 +0500

    My Name Is Khan/  my First Commit

---
+++ LabTask123/src/Labtask321/Task4321.java
@@ -0,0 +1,19 @@
+package Labtask321;
+
+public class Task4321 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("sohail");
+       System.out.println("welcomee   to Bse 7B ....ProjectSahrte");
+	}
+
+}
+
+
+///make project
+///goto team....
+////addd project
+////then first came no head
+////
