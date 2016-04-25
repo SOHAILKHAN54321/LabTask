bf98ed6b84453ebe8d09e5e2dd03f452de672cbc
SOHAIL54321 <sohailajmera1@gmail.com>
2016-04-25 18:04:27 +0500

    My second commit....SNOOOOOOOOKERRRRRRR

--- LabTask123/src/Labtask321/Task4321.java
+++ LabTask123/src/Labtask321/Task4321.java
@@ -7,6 +7,8 @@ public class Task4321 {
 		
 		System.out.println("sohail");
        System.out.println("welcomee   to Bse 7B ....ProjectSahrte");
+       
+       System.out.println("second commit");
 	}
 
 }
@@ -16,4 +18,7 @@ public class Task4321 {
 ///goto team....
 ////addd project
 ////then first came no head
-////
+////from ist time change show question mark
+///then from click index give stric
+////then click on commit and show project code
+///find check sum which have length of 40 charcter
